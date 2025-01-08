4b522f54839a13c8605d18300cfb1f7b27c55bae
bananamen4ik <bananamen4ik@icloud.com>
2025-01-08 03:27:14 +0300

    Added websocket support with example

--- fastapi-core/app/main.py
+++ fastapi-core/app/main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 
 app = FastAPI(
     docs_url=None,
     redoc_url=None
 )
+
+
+@app.websocket("/ws")
+async def ws(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
